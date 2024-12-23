cb5870ba3ebf216581a57d8ee3566feb2a3d58cf
Ryan Silva <ryansilva017@gmail.com>
2024-12-23 12:17:02 -0300

    exception handler e findById

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,12 +2,14 @@ package com.devsuperior.dscatalog.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
-    private static final Long serialVersionUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,19 +1,32 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository repository;
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = repository.findAll();
+        List<CategoryDTO> list2 = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+        return list2;
+    }
+
+    @Transactional(readOnly = true)
+    public  CategoryDTO findById(Long id){
+        Optional<Category> cate = repository.findById(id);
+        Category entity = cate.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+        return new CategoryDTO(entity);
     }
 }
